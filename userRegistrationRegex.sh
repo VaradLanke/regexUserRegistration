0a4f8d678492e388cf589d9678b9a14511e4d1d0
VaradLanke <lanke.varad@gmail.com>
2020-05-05 22:38:59 +0530

    <Refactor> : Added Last Name Validation

--- userRegistrationRegex.sh
+++ userRegistrationRegex.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 echo "Welcome to User Registration"
-read -p "Enter first name" fname
+read -p "First Name : " fname
 fnamepattern="^[A-Z]([a-z]{2})$";
 if [[ $fname =~ $fnamepattern   ]]
 then
   echo "Valid first name";
 else
   echo "Invalid first name";
+fi
+
+read -p "Last Name" lname
+lnamepattern="^[A-Z]([a-z]{2})$";
+if [[ $lname =~ $lnamepattern   ]]
+then
+  echo "Valid last name";
+else
+  echo "Invalid last name";
 fi
