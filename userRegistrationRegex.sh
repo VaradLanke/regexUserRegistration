2e84e20f355e6ed38835bd7254d2a9e43509ef91
VaradLanke <lanke.varad@gmail.com>
2020-05-05 23:00:54 +0530

    <Add> : password Rule3 minNumber

--- userRegistrationRegex.sh
+++ userRegistrationRegex.sh
@@ -39,11 +39,18 @@ fi
 read -p "Enter password" password
 passpat1="^([a-zA-Z0-9@#!]){8}$";
 passpat2="^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$";
+passpat3="^[a-zA-Z@#!]*[0-9]+[a-zA-Z@#!]*$";
+passpat4="^([a-zA-Z0-9]*)[^a-zA-Z_0-9\s]([a-zA-Z0-9]*)$";
 if [[ $password =~ $passpat1   ]]
 then
   if [[ $password =~ $passpat2  ]]
   then
-  	echo "Perfect password"
+     if [[ $password =~ $passpat3  ]]
+     then
+        echo "Perfect password"
+     else
+       echo "Please enter at least one digit";
+     fi
   else
     echo "Please enter at least one caps letter";
   fi
